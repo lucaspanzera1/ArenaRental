c96b1386562b12a9ecf53957d2336d237866ae75
LUCAS_DE_SOUZA_PANZERA <12202690@ALFAB21.BARROCA>
2024-09-27 07:58:49 -0300

    Cadastro de quadras, e fotos ok.

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -8,37 +8,49 @@ session_start();
 $action = isset($_GET['action']) ? $_GET['action'] : '';
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-
+    // Registro da quadra
     if ($action === 'registerQuadra' && isset($_SESSION['client'])) {
         $clientData = $_SESSION['client'];
         $ownerId = $clientData['id'];
 
-        // Captura os dados
+        // Captura os dados do formulário
         $nome = $_POST['nome'];
         $esporte = $_POST['esporte'];
         $quadrac = $_POST['quadrac'] === 'coberta' ? 1 : 0;
-        $rentalType = $_POST['rental-type']; // Captura do tipo de aluguel
+        $rentalType = $_POST['rental-type'];
         $price = $_POST['priceInput'];
 
-        // Registro da quadra
-        $quadra = new Quadra();
-        $quadra->registerQuadra($ownerId, $nome, $esporte, $quadrac, $rentalType, $price);
+        // Registro da quadra e captura do ID
+        // Ao invés de criar um novo objeto Owner, vamos usar um método estático
+        $quadraId = Owner::registerQuadra($ownerId, $nome, $esporte, $quadrac, $rentalType, $price);
 
-        echo "<script type=\"text/javascript\">
-        alert(\"Quadra registrada!\");
+        if ($quadraId) {
+            // Armazena o ID da quadra na sessão
+            $_SESSION['quadra_id'] = $quadraId;
+
+            echo "<script type=\"text/javascript\">
+            alert(\"Quadra registrada com sucesso!\");
+            window.location.href = '../views/owner/imagem.quadra.php?id=" . $quadraId . "';
             </script>";
-        header("refresh: 0.4; url=../views/owner/imagem.quadra.php");
-        exit();
+            exit();
+        } else {
+            echo "<script type=\"text/javascript\">
+            alert(\"Erro ao registrar a quadra. Por favor, tente novamente.\");
+            window.location.href = '../views/owner/register.quadra.php';
+            </script>";
+            exit();
+        }
     }
-    
     if ($action === 'FotoQuadra' && isset($_SESSION['client'])) {
         $clientData = $_SESSION['client'];
-        $owner = Owner::getOwnerById($clientData['id']); // Obtém a instância do Owner
+        $quadraId = $_SESSION['quadra_id']; // Captura o ID da quadra armazenado na sessão
+        $owner = Owner::getOwnerById($clientData['id']);
 
         if ($owner) {
             $origem = isset($_POST['origem']) ? $_POST['origem'] : null;
-            $owner->uploadFotoPerfilOwner($origem); // Chama a função de upload
-            
+            // Associa a foto à quadra usando o ID da quadra
+            $owner->uploadFotoPerfilOwner($quadraId, $origem);
+
             // Redireciona após o upload bem-sucedido
             header("Location: ../views/owner/gerenciador.php");
             exit();
